d6fa1d80d1346b5dd7242001086017b630c86966
Kevin Kim <kevintaekim80@gmail.com>
2018-07-05 14:36:23 -0700

    create initial webpack.config.js file

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  entry: ['./src/index.js'],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    // loaders: [
+    //   {
+    //     exclude: /node_modules/,
+    //     loader: 'babel-loader',
+    //     query: {
+    //       presets: ['react', 'es2015', 'stage-1']
+    //     }
+    //   }
+    // ]
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules | bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ],
+  },
+  // resolve: {
+  //   extensions: ['', '.js', '.jsx']
+  // },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    }
+  }
+};
