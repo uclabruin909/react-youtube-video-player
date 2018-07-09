97a2e7a19d52beab4f1c9e5e40091e46d61afd6e
Kevin Kim <kevintaekim80@gmail.com>
2018-07-09 16:07:40 -0700

    add youtube-api search module and api key

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyDKzyV30L6TS4KdLJ2XEE-pNMyd63P16Qw';
+
 const App = function() {
   return <div>Testing</div>;
 }
