57ae9ea5cdc4022cb4c416bb69a0c394b7a2ab23
Kevin Kim <kevintaekim80@gmail.com>
2018-07-12 16:08:47 -0700

    create initial construction of video list items

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const VideoList = (props) => {
+
+  var videoItems = props.videos.map((video) => {
+    return <li>Video Name: {video.snippet.title}</li>;
+  });
+
   return (
     <ul>
-      {props.videos.length}
+      {videoItems}
     </ul>
   );
 };
