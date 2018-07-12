12aef0a591bfbffbb67fe5f3d4d17786927f6600
Kevin Kim <kevintaekim80@gmail.com>
2018-07-12 15:36:14 -0700

    create initial searchbar state and onChange event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  render() {
-    return <input/>;
+  constructor(props) {
+    super(props);
+    //define initial state of Searchbar
+    this.state = {
+      term: '',
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
   }
+
+  onInputChange(event) {
+    //on input change, update the state.term value
+    this.setState({term: event.target.value});
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={this.onInputChange} />
+        Current state.term value: {this.state.term}
+      </div>
+    );
+  }
+
 }
 
 export default SearchBar;
