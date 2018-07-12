c3cccf146d9429dbef2d1fb061695ec00aa929db
Kevin Kim <kevintaekim80@gmail.com>
2018-07-12 15:46:16 -0700

    convert App component to react class component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDKzyV30L6TS4KdLJ2XEE-pNMyd63P16Qw';
 
-const App = function() {
-  return (
-    <div>
-      <SearchBar/>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar/>
+      </div>
+    );
+  }
+
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
