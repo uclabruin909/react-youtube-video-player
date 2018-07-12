af572b03c9071df4236ca63585cdd051c6bb0cbb
Kevin Kim <kevintaekim80@gmail.com>
2018-07-12 16:01:48 -0700

    import video_list component and render

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDKzyV30L6TS4KdLJ2XEE-pNMyd63P16Qw';
 
@@ -25,6 +26,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar/>
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
