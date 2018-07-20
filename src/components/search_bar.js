a3473489c814cbc5b35e637596851a541349aa48
Kevin Kim <kevintaekim80@gmail.com>
2018-07-19 19:47:49 -0700

    import lodash and debounce the videoSearch functionality

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -20,7 +20,7 @@ class SearchBar extends Component {
   render() {
     return (
       <div className="search-bar">
-        <input value={this.state.term} onChange={this.onInputChange} />
+        <input value={this.state.term} onChange={(event) => this.onInputChange(event)} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -42,9 +43,12 @@ class App extends Component {
   }
 
   render() {
+
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
     return (
       <div>
-        <SearchBar onSearchTermChange={this.videoSearch}/>
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={this.onVideoSelect}
