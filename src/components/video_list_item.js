2ee73d57736ec140809c6b8f38a188d31185024f
Kevin Kim <kevintaekim80@gmail.com>
2018-07-19 18:16:26 -0700

    construct videoListItem element block

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -3,7 +3,21 @@ import React from 'react';
 const VideoListItem = (props) => {
   let video = props.video;
 
-  return <li>Video Name: {video.snippet.title}</li>
+  let imageURL = video.snippet.thumbnails.default.url;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageURL} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
