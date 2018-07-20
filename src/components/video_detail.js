aed0456f9dae6876ed174f9c297ac60ea380f47e
Kevin Kim <kevintaekim80@gmail.com>
2018-07-19 18:28:26 -0700

    update VideoDetail element block with videourl and other properties

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -3,10 +3,12 @@ import React from 'react';
 const VideoDetail = (props) => {
   let video = props.video;
 
+  let videoURL = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
     <div className="col-md-8 video-detail">
       <div className="embed-responsive embed-responsive-16by9">
-        <iframe className="embed-responsive-item"></iframe>
+        <iframe className="embed-responsive-item" src={videoURL}></iframe>
       </div>
 
       <div className="details">
